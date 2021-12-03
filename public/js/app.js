98170ad7653f966d1bd02a9c4033f758487287f3
unknown <ingkaratt@gmail.com>
2021-12-03 17:59:52 +0700

    Remove comment code

--- public/js/app.js
+++ public/js/app.js
@@ -6,18 +6,6 @@ fetch('http://puzzle.mead.io/puzzle').then((res) =>{
     })
 })
 
-// fetch(`http://api.weatherstack.com/current?\
-// access_key=d14f1aaee5a9680eb1062ec02e9c2b60&query=13,100`).then((res) => {
-//     res.json().then((data) => {
-//         if (data.error){
-//             return console.log(data.error.info)
-//         }
-//         const dataCurrent = data.current
-//         const result = `${dataCurrent.weather_descriptions[0]}. It's currently ${dataCurrent.temperature} degress out. It feels like ${dataCurrent.feelslike} degress out.`
-//         console.log(result)
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
