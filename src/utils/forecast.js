ef1e1ca7c73536da8e7d3b87a076d8fe4cb87f97
unknown <ingkaratt@gmail.com>
2021-12-03 19:11:56 +0700

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ access_key=d14f1aaee5a9680eb1062ec02e9c2b60&query=${latitude},${longitude}`
             callback('Unable to find location.', undefined)
         } else {
             const dataCurrent = body.current
-            const result = `${dataCurrent.weather_descriptions[0]}. It's currently ${dataCurrent.temperature} degress out. It feels like ${dataCurrent.feelslike} degress out.`
+            const result = `${dataCurrent.weather_descriptions[0]}. It's currently ${dataCurrent.temperature} degress out. It feels like ${dataCurrent.feelslike} degress out. The Humidity is ${dataCurrent.humidity}%.`
             callback(undefined, result)
         }
     })
